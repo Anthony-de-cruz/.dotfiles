5b854c8436214370ab2816b61b23ee28c7cf09ed
Anthony de Cruz <anthonydecruz@proton.me>
2024-01-14 13:16:28 +0000

    added mason-lspconfig

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -10,7 +10,37 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "pyright", "html", "cssls", "tsserver", "rust_analyzer", "clangd", "jdtls" },
+				ensure_installed = {
+					"pyright",
+					"html",
+					"cssls",
+					"tsserver",
+					"rust_analyzer",
+					"clangd",
+					"jdtls",
+				},
+			})
+		end,
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			require("mason-null-ls").setup({
+				ensure_installed = {
+					-- Opt to list sources here, when available in mason.
+                    "stylua",
+                    "prettier",
+                    "black",
+                    "google-java-format",
+                    "clang-format",
+				},
+				automatic_installation = false,
+				handlers = {},
 			})
 		end,
 	},
