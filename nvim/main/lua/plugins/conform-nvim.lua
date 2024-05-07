9408111fd1be65f1bb8374012c0d7f8dc82ef3ae
Anthony de Cruz <anthonydecruz@proton.me>
2024-05-07 09:40:32 +0100

    replaced none-ls with confirm-nvim

---
+++ nvim/main/lua/plugins/conform-nvim.lua
@@ -0,0 +1,31 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				-- Conform will run multiple formatters sequentially
+				-- Use a sub-list to run only the first available formatter
+				lua = { "stylua" },
+				python = { "black" },
+				typescript = { { "prettierd", "prettier" } },
+				javascript = { { "prettierd", "prettier" } },
+				json = { { "prettierd", "prettier" } },
+				html = { { "prettierd", "prettier" } },
+				css = { { "prettierd", "prettier" } },
+				scss = { { "prettierd", "prettier" } },
+				markdown = { { "prettierd", "prettier" } },
+			},
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+			-- Use the "*" filetype to run formatters on all filetypes.
+			-- ["*"] = { "codespell" },
+			-- Use the "_" filetype to run formatters on filetypes that don't
+			-- have other formatters configured.
+			["_"] = { "trim_whitespace" },
+		})
+	end,
+}
