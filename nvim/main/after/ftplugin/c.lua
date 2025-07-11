b63b7569574777247e09c273404872a681ba7790
Anthony de Cruz <anthonydecruz@proton.me>
2025-07-11 20:15:32 +0100

    Added c specific nvim config.

---
+++ nvim/main/after/ftplugin/c.lua
@@ -0,0 +1,6 @@
+vim.opt_local.tabstop = 8
+vim.opt_local.softtabstop = 8
+vim.opt_local.shiftwidth = 8
+vim.opt_local.expandtab = false -- use tabs instead of spaces
+
+vim.opt_local.colorcolumn = "96"
